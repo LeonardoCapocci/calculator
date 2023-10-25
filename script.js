2eb4e00dcab08378837b541ca0e0b3eb5a38b456
LeonardoCapocci <leocap2001@gmail.com>
2023-10-25 14:07:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(currentTotal=0, addition) {
+  return currentTotal + addition
+}
+
+function subtract(currentTotal=0, subtraction) {
+  return currentTotal - subtraction
+}
+
+function multiply(currentTotal=0, multiplication) {
+  return currentTotal * multiplication
+}
+
+function divide(currentTotal=0, division) {
+  return currentTotal / division
+}
+
+function operate(operator, a, b) {
+  
+}
