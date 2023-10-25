be54930fd796ccaf893ad64526b33896004cf393
LeonardoCapocci <leocap2001@gmail.com>
2023-10-25 16:01:49 -0700

    completed the style of the calculator

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let firstOperand = ''
+let secondOperand = ''
+let operator = ''
+
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
+const currentOperationScreen = document.querySelector('.current-operation')
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', currentOperationScreen.textContent += button.textContent)
+})
+
 function add(currentTotal=0, addition) {
   return currentTotal + addition
 }
@@ -14,6 +26,19 @@ function divide(currentTotal=0, division) {
   return currentTotal / division
 }
 
-function operate(operator, x, y) {
-
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case '+':
+      add(firstOperand, secondOperand)
+      break
+    case '-':
+      subtract(firstOperand, secondOperand)
+      break
+    case 'x':
+      multiply(firstOperand, secondOperand)
+      break
+    case '÷':
+      divide(firstOperand, secondOperand)
+      break
+  }
 }
