830bef6aab260843f5d488fa9d381d8d4fda54be
LeonardoCapocci <leocap2001@gmail.com>
2023-10-26 11:25:54 -0700

    numbers can be input to the calc

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let firstOperand = ''
 let secondOperand = ''
 let operator = ''
 let displayValue = '0'
-
 const buttons = document.querySelectorAll('button')
 const currentOperationScreen = document.querySelector('.current-operation')
 
@@ -12,8 +11,22 @@ function updateDisplay() {
 }
 updateDisplay()
 
+function clickButton() {
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('operand')) {
+        inputOperand(button.value)
+        updateDisplay()
+      }
+    })
+  })
+}
+clickButton()
 
-
+function inputOperand(operand) {
+  if (displayValue == '0') displayValue = operand
+  else displayValue += operand
+}
 
 function operate(operator, firstOperand, secondOperand) {
   switch (operator) {
